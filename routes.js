15f8f971ea86f8d15c2b909ed77a0cf5249ac0c3
Hritvik16 <hvenkateshwa@umass.edu>
2019-05-13 15:29:06 -0400

    gonna make this my protfolio page

--- routes.js
+++ routes.js
@@ -7,4 +7,14 @@ router.get("/", function(rec, res) {
     res.render("index");
 });
 
+router.get("/about", function(rec, res) {
+    console.log("Hello World");
+    res.render("about");
+});
+
+router.get("/Log%20In", function(rec, res) {
+    console.log("Hello World");
+    res.render("login");
+});
+
 module.exports = router;
